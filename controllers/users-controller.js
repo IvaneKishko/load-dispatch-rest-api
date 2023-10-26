f4ff61d0ac5efb310f95808e4bc79f3ccf99b927
iva_kishko <ivakishko@gmail.com>
2023-10-26 20:01:22 +0400

    replace bcrypt with bcryptjs

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,5 +1,5 @@
 const { validationResult } = require("express-validator");
-const bcrypt = require("bcryptjs");
+const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
 
 const HttpError = require("../models/http-error");
