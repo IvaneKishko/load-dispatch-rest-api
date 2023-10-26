48b0dbe50379c0daab22cd8f4cb94dea705fe929
iva_kishko <ivakishko@gmail.com>
2023-10-26 17:24:59 +0400

    update mongo string

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.q13yfea.mongodb.net/${process.env.NAME}?retryWrites=true&w=majority`
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.q13yfea.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then(() => app.listen(process.env.PORT || 5000))
   .catch((err) => {
